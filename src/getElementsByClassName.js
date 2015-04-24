8737eb362e1160b4bd2c7277238860e4d0de21ef
Danielle Knudson <knudson.danielle@gmail.com>
2015-04-24 13:54:13 -0700

    Committing my most recent changes to src/getElementsByClassName.js which passes the spec. Uses a document traversal method.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  // you should use document.body, element.childNodes, and element.classList
+  
+  var elementsWithClassName = [];
+
+  // create a function which traverses the document body
+  var traverseDocument = function(node){
+  	node = node || document.body;
+
+  	// first check to see if node has className
+  	if (node.classList.contains(className)) {
+  		elementsWithClassName.push(node);
+  	}
+
+  	// now check to see if node has children,
+  	// and if so, if they have the className
+  	if (node.childNodes.length !== 0) {
+  		var children = node.childNodes;
+  		for (var i = 0; i < children.length; i++) {
+  			if (children[i].nodeType === 1) {
+  				traverseDocument(children[i]);
+  			}
+  		}
+  	}   
+  };
+
+  traverseDocument();
+  return elementsWithClassName;
+
 };
